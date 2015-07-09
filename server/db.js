35acbe9d35782ba4039035822d752a6cccae5ebb
Victor Savu <victorsavu3@gmail.com>
2015-07-09 14:42:32 +0200

    document db manager

--- server/db.js
+++ server/db.js
@@ -1,12 +1,27 @@
 /* global setInterval */
 /* global setImmediate */
 
+/** @namespace server.db */
+
 var sqlite3 = require('sqlite3').verbose();
 var _ = require('lodash');
 
+/** @external SQLiteDB
+  * @see https://github.com/mapbox/node-sqlite3 */
+
+/** @type {Object.<String,Array.<SQLiteDB>>}
+    @memberof server.db
+    @summary This cache keeps multiple databases open in order to reduce access
+              time for new queries. The open databases are closed at a rate of
+              1/s and are opened whenever one is not available.
+ */
 var cache = {};
 
-setInterval(function() {
+/** @memberof server.db
+    @summary This function handles the cleanup of open databases.
+    @private
+*/
+function cleanupDB() {
   _.forEach(cache, function(dbarr, id) {
     if (dbarr.length > 0) {
       var db = dbarr.pop();
@@ -15,8 +30,24 @@ setInterval(function() {
       console.log('Closing ' + id + ' (' + dbarr.length + ' remaining)');
     }
   });
-}, 1000);
+}
 
+setInterval(cleanupDB, 1000);
+
+/** @callback DBCallback
+    @memberof server.db
+    @param {String} err
+    @param {SQLiteDB} db
+ */
+
+/** @memberof server.db
+    @summary Open a db and replace the closing function to simply add it to
+              the pool.
+    @private
+    @param {String} id The name of the database.
+    @param {server.db.DBCallback} cb
+    @return {SQLiteDB}
+*/
 function openDb(id, cb) {
   var path = './data/' + id + '.db';
 
@@ -35,6 +66,12 @@ function openDb(id, cb) {
   return db;
 }
 
+/** @memberof server.db
+    @summary Get a open SQLiteDB. It is only abailable after cb fires.
+    @param {String} id The name of the database.
+    @param {server.db.DBCallback} cb
+    @return {SQLiteDB}
+*/
 function dbManager(id, cb) {
   if (!cache[id]) {
     cache[id] = [];
