f71bf1afdb6bddb1c6420e66cd91858edb6697bc
Victor Savu <victorsavu3@gmail.com>
2015-10-23 22:30:02 +0200

    set up testing environment and add database tests

--- app/tests/config.js
+++ app/tests/config.js
@@ -1,3 +1,9 @@
 var expect = chai.expect;
 
 mocha.ui('bdd');
+
+var $injector = angular.injector(['ng', 'app']);
+
+var loader = $injector.get('LoaderService');
+
+chai.should();
--- app/tests/db.js
+++ app/tests/db.js
@@ -1,5 +1,25 @@
-describe('A suite', function() {
-  it('contains spec with an expectation', function() {
-    expect(true).to.be.equal(true);
+describe('Databases', function() {
+  it('contain euinit.db', function() {
+    return loader.getRuns().should.eventually.contain('eunit');
+  });
+});
+
+describe('eunit.db', function() {
+  before(function() {
+    loader.setRun('eunit');
+  });
+
+  it('should contain 4 calls', function() {
+    return loader.getCalls().should.eventually.have.length(4);
+  });
+
+  it('should contain 6 references', function() {
+    return loader.getReferences().should.eventually.have.length(6);
+  });
+
+  it('should have a call group', function() {
+    return loader.getCall('0_2').then(function(call) {
+      return call.getCallGroup();
+    }).should.eventually.have.property('id', 1);
   });
 });
