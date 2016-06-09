a8789f086c69fef65ff075a802cc8973c2f5f86b
Savu Victor <victorsavu3@gmail.com>
2016-06-09 11:42:06 +0200

    also group callgroups by thread

--- database/06.call_group.sql
+++ database/06.call_group.sql
@@ -2,7 +2,7 @@ WITH ThreadTimings AS (
   SELECT thr.TSCPerMillisecond AS TSCPerMillisecond, thr.StartTime AS StartTime FROM Thread thr WHERE thr.Id = c.Thread LIMIT 1
 )
 INSERT INTO CallGroup (Function, Caller, Count, Parent, Start, End, StartTime,
-  EndTime, Duration, DurationMs, CallerExecution) SELECT
+  EndTime, Duration, DurationMs, CallerExecution, Thread) SELECT
   Function,
   Caller,
   COUNT(*),
@@ -13,5 +13,6 @@ INSERT INTO CallGroup (Function, Caller, Count, Parent, Start, End, StartTime,
   strftime('%Y-%m-%dT%H:%M:%fZ', (SELECT StartTime FROM ThreadTimings), '+' || CAST ((MAX(Start) / (SELECT TSCPerMillisecond FROM ThreadTimings) / 1000) AS TEXT) || ' seconds'),
   SUM(End - Start),
   SUM(End - Start) / (SELECT TSCPerMillisecond FROM ThreadTimings),
-  CASE WHEN MIN(CallerExecution) = MAX(CallerExecution) THEN CallerExecution ELSE NULL END AS CallerExecution
-FROM Call c GROUP BY Function, Caller;
+  CASE WHEN MIN(CallerExecution) = MAX(CallerExecution) THEN CallerExecution ELSE NULL END AS CallerExecution,
+  Thread
+FROM Call c GROUP BY Function, Caller, Thread;
