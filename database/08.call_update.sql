c7d8acd132019c59feaa54e9f7921c6b445d51fc
Savu Victor <victorsavu3@gmail.com>
2016-06-09 11:51:44 +0200

    fix callgroup field

--- database/08.call_update.sql
+++ database/08.call_update.sql
@@ -1,5 +1,5 @@
-UPDATE Call SET CallGroup=(SELECT g.Id FROM CallGroup g WHERE g.Caller = Call.Caller AND g.Function = Call.Function) WHERE Call.Caller IS NOT NULL;
-UPDATE Call SET CallGroup=(SELECT g.Id FROM CallGroup g WHERE Call.Function = g.Function) WHERE Call.Caller IS NULL;
+UPDATE Call SET CallGroup=(SELECT g.Id FROM CallGroup g WHERE g.Caller = Call.Caller AND g.Function = Call.Function AND Call.Thread = g.Thread) WHERE Call.Caller IS NOT NULL;
+UPDATE Call SET CallGroup=(SELECT g.Id FROM CallGroup g WHERE Call.Function = g.Function AND Call.Thread = g.Thread) WHERE Call.Caller IS NULL;
 UPDATE Call SET CallsOther=(SELECT COUNT(t.Id) FROM Call t WHERE t.Caller = Call.Id );
 UPDATE CallGroup SET Parent=(SELECT c.CallGroup FROM Call c WHERE c.Id = CallGroup.Caller);
 
